b2e23d60451b29b1ae5c3ac8f747833ac46355d0
TrongThang <it.ptthang@gmail.com>
2024-04-20 18:08:59 +0700

    fix

--- ManagementSupermarket/Manager/frmOrder.cs
+++ ManagementSupermarket/Manager/frmOrder.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DTO;
+using BLL;
+using static System.Windows.Forms.ListViewItem;
 
 namespace ManagementSupermarket
 {
@@ -23,6 +26,47 @@ namespace ManagementSupermarket
             InitializeComponent();
         }
 
+        Event eventConfig = new Event();
+        //LOAD DATA COMBO BOX
+        private void LoadDataComboBox_NameProduct()
+        {
+            cbb_NameProductCreate.DataSource = (new BLL_Product()).GetProduct("MaSP");
 
+            cbb_NameProductCreate.DisplayMember = "TenSP";
+            cbb_NameProductCreate.ValueMember = "MaSP";
+        }
+
+        private void frmOrder_Load(object sender, EventArgs e)
+        {
+            LoadDataComboBox_NameProduct();
+        }
+
+        private void lst_OrderCurrency_Click(object sender, EventArgs e)
+        {
+            if (lst_OrderCurrency.SelectedItems.Count > 0)
+            {
+                ListViewSubItemCollection itemSelected = lst_OrderCurrency.SelectedItems[0].SubItems;
+
+                cbb_NameProductCreate.Text = itemSelected[0].Text;
+                num_CountProductCreate.Text = itemSelected[1].Text;
+                txt_Price.Text = itemSelected[2].Text;
+                cbb_DiscountCreate.Text = itemSelected[3].Text;
+                cbb_DiscountCreate.Text = itemSelected[4].Text;
+                txt_AmountCreate.Text = itemSelected[5].Text;
+            }
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            string nameProduct, price, idDiscount;
+            decimal count, discount, amount;
+
+            nameProduct = cbb_NameProductCreate.SelectedValue.ToString();
+            count = num_CountProductCreate.Value;
+            price = txt_PriceCreate.Text;
+            idDiscount = cbb_DiscountCreate.SelectedValue.ToString();
+            discount = (new )
+
+        }
     }
 }
