4b2135acf1373d0e360044f49af5c36a312a6c47
TrongThang <it.ptthang@gmail.com>
2024-04-25 12:57:03 +0700

    Fix master

--- ManagementSupermarket/Program.cs
+++ ManagementSupermarket/Program.cs
@@ -18,7 +18,7 @@ namespace ManagementSupermarket
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-             Application.Run(new frmWarehouse());
+             Application.Run(new frmOrder());
 
         }
     }
